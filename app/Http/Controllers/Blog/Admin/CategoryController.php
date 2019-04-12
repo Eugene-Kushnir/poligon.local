a8ce2f45c0e793539af13a1671e5f74ae4d365e6
Kushnir Eugene <kushnir.fort@gmail.com>
2019-04-13 00:58:00 +0300

    create Reposotory for Category

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Blog\Admin;
 use App\Http\Requests\BlogCategoryCreateRequest;
 use App\Http\Requests\BlogCategoryUpdateRequest;
 use App\Models\BlogCategory;
+use App\Repositories\BlogCategoryRepository;
 
 class CategoryController extends BaseController
 {
@@ -67,10 +68,16 @@ class CategoryController extends BaseController
      * @return \Illuminate\Http\Response
      */
 
-    public function edit($id)
+    public function edit($id, BlogCategoryRepository $categoryRepository)
     {
-		$item = BlogCategory::findOrFail($id);
-		$categoryList = BlogCategory::all();
+//		$item = BlogCategory::findOrFail($id);
+//		$categoryList = BlogCategory::all();
+
+		$item = $categoryRepository->getEdit($id);
+		if (empty($item)) {
+			abort(404);
+		}
+		$categoryList = $categoryRepository->getForComboBox();
 
 		return view('blog.admin.categories.edit',
 			compact('item','categoryList'));
